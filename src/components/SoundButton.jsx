2e05d2d2b3d94353648a50d17b630e3893f72f20
DMITRII POPOV <bulpopov@gmail.com>
2023-06-13 13:17:05 +0300

    sound button now looks like a button

--- src/components/SoundButton.jsx
+++ src/components/SoundButton.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-
+import { FaVolumeUp, FaVolumeMute } from 'react-icons/fa';
 
 function SoundButton(props) {
-  return (
-    <button className="soundsbutton" onClick={props.onClick}>
-      {props.isSoundOn ? 'sounds off' : 'sounds on'}
-    </button>
-  );
+    return (
+        <button className="soundsbutton" onClick={props.onClick}>
+            {props.isSoundOn ? <FaVolumeUp /> : <FaVolumeMute />}
+        </button>
+    );
 }
 
 export default SoundButton;
